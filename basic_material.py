a85c63de7f09e25b68716aacc4971d3b61ae47bf
JonHowMIT <52931333+JonHowMIT@users.noreply.github.com>
2022-10-23 16:17:30 -0400

    sun aft

--- basic_material.py
+++ basic_material.py
@@ -1,45 +1,52 @@
 import shutil, sys, os.path, math, time
 
 import numpy as np
+from numpy import logspace, linspace
 float_formatter = "{:.4f}".format
 np.set_printoptions(formatter={'float': '{: 8.3f}'.format})
 
-#%matplotlib inline
 import matplotlib
 import matplotlib.cm as cm
 from matplotlib.cm import get_cmap
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure, savefig
 from matplotlib import gridspec
 from matplotlib import rcParams
 rcParams["font.serif"] = "cmr14"
-rcParams['savefig.dpi'] = 300
-rcParams["figure.dpi"] = 100
 rcParams.update({'font.size': 18})
-rcParams['axes.grid'] = True
-rcParams['lines.linewidth'] = 2.0
- 
-#font = {'family': 'arial','color':  'darkred','weight': 'normal','size': 18}
-#plt.rcParams['figure.figsize'] = [12, 8.0]
-#plt.rcParams['figure.dpi'] = 300
-#plt.rcParams.update({"axes.grid" : True, "grid.color": "grey"})
-#plt.rcParams['grid.linestyle'] = ':'
+plt.rcParams['figure.figsize'] = [8, 5.0]
+plt.rcParams['figure.dpi'] = 150
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['axes.xmargin'] = 0
+plt.rcParams['axes.grid'] = True
 
-
-from IPython.display import display, Markdown
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
-from IPython.display import display
 import sympy
 import sympy as sym
 
-#from pyomo.environ import *
-
-import scipy
-from scipy import optimize 
-from scipy.optimize import fsolve, line_search, minimize, LinearConstraint, NonlinearConstraint, Bounds
+from scipy import signal
+from scipy.fft import fft, fftfreq, fftshift, ifft
+from scipy.signal import blackman
 
 import warnings
 warnings.filterwarnings("ignore", message="delta_grad == 0.0. Check if the approximated function is linear.")
 
 from platform import python_version
 print("Running Python:",python_version())
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+def nicegrid(ax=plt):
+    ax.grid(True, which='major', color='#666666', linestyle=':')
+    ax.grid(True, which='minor', color='#999999', linestyle=':', alpha=0.2)
+    ax.minorticks_on()
